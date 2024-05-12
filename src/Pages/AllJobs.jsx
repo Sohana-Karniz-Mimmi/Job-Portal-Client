2de8b13a63cdcb53520a50c8647e8b7d7b4367f5
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-05-13 02:47:56 +0600

    added modal

--- src/Pages/AllJobs.jsx
+++ src/Pages/AllJobs.jsx
@@ -4,14 +4,7 @@ import Navbar from "../Components/Navbar";
 import Table from "../Components/Table";
 import axios from "axios";
 
-import { useRef } from 'react';
-import generatePDF from 'react-to-pdf';
-
 const AllJobs = () => {
-
-    const targetRef = useRef();
-
-
     const [search, setSearch] = useState('')
     const [searchText, setSearchText] = useState('')
     const [jobs, setJobs] = useState([])
@@ -57,21 +50,14 @@ const AllJobs = () => {
                 <div className="text-center">
                     <h2 className="font-semibold text-3xl lg:text-[44px] lg:mb-4 mb-2"> All Jobs</h2>
                     <p className="lg:w-[550px] lg:text-lg text-[#9ca3a9] font-medium md:w-[500px] text-sm  w-80 mx-auto">Discover Our Newest Listings: The Latest Properties Ready to Impress and Inspire Your Next Adventure in Real Estate Excellence!</p>
-                </div>
-
-                <div>
-                    <button className="btn" onClick={() => generatePDF(targetRef, { filename: 'https://drive.google.com/file/d/1DcrtNBJ7Br7gLD25z6dNLTZs2NRftZft/view?usp=sharing' })}>Download PDF</button>
-                    <div ref={targetRef}>
-                        Content to be included in the PDF
-                    </div>
-                </div>
+                </div>     
 
                 {/* Sort By*/}
                 <div className='flex flex-col md:flex-row justify-center items-center gap-5 py-4'>
 
                     {/* Search */}
                     <form
-                        onSubmit={handleSearch}
+                    onSubmit={handleSearch}
                     >
                         <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
                             <input
@@ -89,7 +75,7 @@ const AllJobs = () => {
                             </button>
                         </div>
                     </form>
-
+                    
                     <button
                         onClick={handleReset}
                         className='btn'
